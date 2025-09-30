a9b2a0ee452e9bc13fc9e09e5ad0d5612e88b4f5
arajflcc <arajfingerlakes@gmail.com>
2025-09-30 20:31:45 +0000

    update my solution

--- compliment_01.py
+++ compliment_01.py
@@ -1,8 +1,8 @@
 # FILE NAME - compliment_01.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
+# NAME: Anita Raj   
+# DATE: 09/30/2025
+# BRIEF DESCRIPTION:  Generate a code giving compliment
 
 
 
@@ -14,7 +14,10 @@
 
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
-
+user_answer = input("Would you like a compliment? ")
+if user_answer == "yes":
+    print("You have wonderful eyes.")
+print("Thank you for playing.") 
 
 
 
@@ -66,12 +69,12 @@ Thank you for playing.
 '''
 
 1. On a scale of 1 to 10 (where 10 is the hardest), how would you rate this lab?
-
+I would rate this lab a 4.  It wasn't too hard nor too easy.
 
 
 
 2. What was the hardest part of this lab?
-
+Need to know when to use elif and else.  Did this lab without either of the function(else & elif)
 
 
 
